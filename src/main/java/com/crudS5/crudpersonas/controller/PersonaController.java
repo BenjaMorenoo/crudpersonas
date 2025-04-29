29c4ed8801ceb5cc183f6a2898eebde0efd4f85e
DonHuea <benjaben100@gmail.com>
2025-04-29 12:16:06 -0400

    Creacion controller

---
+++ src/main/java/com/crudS5/crudpersonas/controller/PersonaController.java
@@ -0,0 +1,20 @@
+package com.crudS5.crudpersonas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/api/personas")
+public class PersonaController {
+    
+    @GetMapping
+    public String saludar() {
+        return "api funcionando";
+    }
+
+}
+
+
